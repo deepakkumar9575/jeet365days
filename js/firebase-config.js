d3b9c246c9041503ffb585ca8f98283a6f08822f
deepakkumar9575 <gindoripiplya@gmail.com>
2025-07-18 08:15:31 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAIazwoh9hesNYpT5ywc-aX3qnj4UcGhKY",
   authDomain: "jeet365-fabf2.firebaseapp.com",
   databaseURL: "https://jeet365-fabf2-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "jeet365-fabf2",
-  storageBucket: "jeet365-fabf2.firebasestorage.app",
+  storageBucket: "jeet365-fabf2.appspot.com",
   messagingSenderId: "862480702968",
   appId: "1:862480702968:web:fe41c67573c82cb7ec4a0d",
   measurementId: "G-LZRXW175NR"
@@ -16,7 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
